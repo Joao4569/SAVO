92b0c158857abdb7cf2028a9a78ba390a72c67f0
João Rodrigues <johnnyrod4569@gmail.com>
2024-11-28 14:35:34 +0100

    fix: Squashed bug with accordion icon triggering on info_pet_owner.html.

--- static/js/accordion-button-reaction.js
+++ static/js/accordion-button-reaction.js
@@ -1,14 +1,20 @@
 document.addEventListener("click", function (event) {
-  if (event.target.closest(".testing")) {
-    const iconElement = event.target.closest(".testing").querySelector("i");
+  if (event.target.closest(".testing-two")) {
+    const testingElement = event.target
+      .closest(".testing-two")
+      .querySelector(".testing");
 
-    if (iconElement) {
-      if (iconElement.classList.contains("fa-circle-chevron-down")) {
-        iconElement.classList.remove("fa-circle-chevron-down");
-        iconElement.classList.add("fa-circle-chevron-up");
-      } else if (iconElement.classList.contains("fa-circle-chevron-up")) {
-        iconElement.classList.remove("fa-circle-chevron-up");
-        iconElement.classList.add("fa-circle-chevron-down");
+    if (testingElement) {
+      const iconElement = testingElement.querySelector("i");
+
+      if (iconElement) {
+        if (iconElement.classList.contains("fa-circle-chevron-down")) {
+          iconElement.classList.remove("fa-circle-chevron-down");
+          iconElement.classList.add("fa-circle-chevron-up");
+        } else if (iconElement.classList.contains("fa-circle-chevron-up")) {
+          iconElement.classList.remove("fa-circle-chevron-up");
+          iconElement.classList.add("fa-circle-chevron-down");
+        }
       }
     }
   }
